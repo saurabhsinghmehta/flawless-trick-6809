8984b0a6aaf49d0cb9df35677d43a41d78eff1fc
Saurabh Singh <saurabhsingh.bba17@gmail.com>
2022-12-15 13:48:43 +0530

    files added

--- src/Pages/AdminDashboard.jsx
+++ src/Pages/AdminDashboard.jsx
@@ -1,12 +1,23 @@
 import { Flex, Grid, GridItem } from "@chakra-ui/react";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import DisplayProducts from "../Components/DisplayProducts";
 import Sorting from "../Components/Sorting";
 import "../CSS/admin.css";
+import { getData } from "../Redux/Sorting/actiontype";
+import { useEffect } from "react";
+import { store } from "../Redux/store";
+
 function AdminDashboard() {
-  const data = useSelector((e) => e.data);
-  // console.log(data);
+  console.log(store)
+  const data = useSelector((e) => e.Sortingreducer.data);
+  console.log(data);
+
+  const dispatch=useDispatch();
+
+  useEffect(() => {
+   dispatch(getData("biryani"))
+  }, []);
   return (
     //display sales status on daily, weekly and monthly bases
     <>
@@ -33,4 +44,4 @@ function AdminDashboard() {
   );
 }
 
-export default AdminDashboard;
+export default AdminDashboard;
