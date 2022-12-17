c3ef9da7b019ae93b73492a7613360bf6a9b94d0
Vivek-bhatt05 <vivekbhatt529@gmail.com>
2022-12-17 15:49:00 +0530

    deals part added

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -8,12 +8,14 @@ import Home from "../Pages/Home";
 import Login from "../Pages/Login";
 import Otp from "../Pages/Otp";
 import Cart from "../Pages/Cart";
+import Deals from "../Pages/Deals";
 function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/menu" element={<Menu />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/deals" element={<Deals />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/otp" element={<Otp />} />
       <Route path="/admin" element={<AdminDashboard />} />
